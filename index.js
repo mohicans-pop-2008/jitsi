c15dd4a7134031ea4e55d61a401921591e471f08
Kevin Hu <hukxchen+git@gmail.com>
2020-10-08 09:48:27 -0500

    feat: use lib-jitsi-meet

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-const domain = 'meet.jit.si';
+// import JitsiMeetJS from "@lyno/lib-jitsi-meet";
+
+JitsiMeetJS.init();
+
 const options = {
     roomName: 'FULLSTACKACADEMYEXAMPLE',
     width: 700,
     height: 700,
     parentNode: document.querySelector('#meet')
 };
-const api = new JitsiMeetExternalAPI(domain, options);
+
+const connection = new JitsiMeetJS.JitsiConnection(null, null, {})
+
+// const domain = 'meet.jit.si';
+// const options = {
+//     roomName: 'FULLSTACKACADEMYEXAMPLE',
+//     width: 700,
+//     height: 700,
+//     parentNode: document.querySelector('#meet')
+// };
+// const api = new JitsiMeetExternalAPI(domain, options);
